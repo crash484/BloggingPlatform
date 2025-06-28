e7472c9f0c73ba4a18044fb3d92ea80af479df67
Shashwat Jain <129047199+crash484@users.noreply.github.com>
2025-06-28 21:17:41 +0530

    added models for user, post, comments, jwt middleware

---
+++ server/models/BlogModel.js
@@ -0,0 +1,34 @@
+import mongoose from 'mongoose';
+
+const blogSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  content: {
+    type: String, // Rich text stored as HTML from Quill
+    required: true
+  },
+  imageUrl: String,
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  categories: [{
+    type: String
+  }],
+  likes: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }],
+  comments: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Comment'
+  }]
+}, {
+  timestamps: true
+});
+
+const Blog = mongoose.model('Blog', blogSchema);
+export default Blog;
